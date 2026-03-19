3261f5433f973982a262978e1bd24c471c5fa31c
it24101661 <it24101661@my.sliit.lk>
2026-03-19 10:55:24 +0530

    add comment

--- product-listing/src/main/java/com/vegwaste/productlisting/ProductListingApplication.java
+++ product-listing/src/main/java/com/vegwaste/productlisting/ProductListingApplication.java
@@ -3,7 +3,7 @@ package com.vegwaste.productlisting;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//comment
 @SpringBootApplication
 public class ProductListingApplication {
     public static void main(String[] args) {
