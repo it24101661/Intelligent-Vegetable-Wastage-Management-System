b14faa1afa10d2c03806aabc06f7a22bbe8b9c07
IT24102412 <IT24102412@my.sliit.lk>
2026-03-19 12:39:51 +0530

    Add files via upload

---
+++ DeliveryAndDistributionApplication.java
@@ -0,0 +1,13 @@
+package com.example.delivery_and_distribution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryAndDistributionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryAndDistributionApplication.class, args);
+    }
+
+}
